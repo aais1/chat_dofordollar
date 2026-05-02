c10b831a22fd9dc225849843f27a363c5b8446bc
aais1 <aaisali228@gmail.com>
2026-05-03 03:03:38 +0500

    add

--- server/index.js
+++ server/index.js
@@ -24,16 +24,19 @@ const httpServer = createServer(app);
 
 // CORS
 const allowedOrigins = [
-  process.env.CLIENT_URL || 'http://localhost:5173',
-  'http://localhost:5173',
-  'http://localhost:3000',
   'https://chatapp.dofordollars.com',
-  'http://chatapp.dofordollars.com'
+  'http://localhost:5173',
 ];
 
 app.use(cors({
-  origin: (origin, callback) => {
-    callback(null, true);
+  origin: function (origin, callback) {
+    if (!origin) return callback(null, true);
+
+    if (allowedOrigins.includes(origin)) {
+      return callback(null, true);
+    }
+
+    return callback(new Error("Not allowed by CORS"));
   },
   credentials: true,
 }));
